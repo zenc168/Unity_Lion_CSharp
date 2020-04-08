3ee44e81f4321709736bfab9ff6d02cc285aa74e
zenc168 <63327387+zenc168@users.noreply.github.com>
2020-04-08 15:06:54 +0800

    Add mp4

--- Csharp/Assets/Car.cs
+++ Csharp/Assets/Car.cs
@@ -2,6 +2,8 @@
 
 // "//" to elaborate, only seen here
 // "///" to summary, can be seen at other classes
+// coding is class
+// In Unity is object
 /// <summary>
 /// to define car object
 /// </summary>
@@ -13,13 +15,35 @@ public class Car : MonoBehaviour
     // weight-100.9- float 
     // brand-bmw- string
     // brake-y/n- bool
-
+    // only 'public' will show in Inspector in Unity 
+    // default is 'private'
     // how to define
     // initial value int, float 0, string "", bool false
-    int cc = 2000;
-    float weight = 100.9f;
-    string brand = "BMW";
-    bool brake = false;
+    //Range is for number types only [Range(min, max)]
+    /// <summary>
+    /// Car's cc number summary
+    /// </summary>
+    [Header("Car's cc number"), Range(500, 5000)]
+    public int cc = 2000;
+    
+    /// <summary>
+    /// Car's weight summary
+    /// </summary>
+    [Header("Car weight")]
+    [Tooltip("This is car's weight"), Range(50, 350)]
+    public float weight = 100.9f;
 
+    /// <summary>
+    /// Car's brand summmary
+    /// </summary>
+    [Header("Car's brand name")]
+    [Tooltip("This is car's brand")]
+    public string brand = "BMW";
+
+    /// <summary>
+    /// Car's brake summmary boolean
+    /// </summary>
+    [Header("brake on/off"), Tooltip("Do you have hand brake on")]
+    public bool brake = false;
 
 }
--- Csharp/Assets/Class1.cs
+++ Csharp/Assets/Class1.cs
@@ -9,9 +9,27 @@ public class Class1 : MonoBehaviour
 
     // event: at specified timestamp, method to be executed number of times
     // start(): execute once when started, initial setting
+    
+    
+    [Header("Car number #1#")]
+    public Car car1;
+
+    [Header("Car number #2#")]
+    public Car car2;
 
     private void Start()
     {
         print("Hello Zen");
+
+        //retrieve
+        print("Car1's cc number is "+car1.cc);
+        print("Car2's brand is "+car2.brand);
+        //set
+        car1.cc = 999;
+        car1.weight = 999f;
+        car1.brand = "Aston Martin";
+        car1.brake = true;
+        
+
     }
 }
