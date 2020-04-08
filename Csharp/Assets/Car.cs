767a0b93aba3a28528c8393089b9e6fc8f03a728
zenc168 <63327387+zenc168@users.noreply.github.com>
2020-04-08 11:53:31 +0800

    Add C#

---
+++ Csharp/Assets/Car.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+// "//" to elaborate, only seen here
+// "///" to summary, can be seen at other classes
+/// <summary>
+/// to define car object
+/// </summary>
+
+public class Car : MonoBehaviour
+{
+    // field :  to store data
+    // cc -2000- int
+    // weight-100.9- float 
+    // brand-bmw- string
+    // brake-y/n- bool
+
+    // how to define
+    // initial value int, float 0, string "", bool false
+    int cc = 2000;
+    float weight = 100.9f;
+    string brand = "BMW";
+    bool brake = false;
+
+
+}
---
+++ Csharp/Assets/Class1.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;    //using Unity API
+
+// class is to define a object
+// : MonoBehaviour can be used by Unity Object 
+public class Class1 : MonoBehaviour
+{
+    // 1) When define class, case mattters
+    // 2) {} () [] '' "" < > comes in pair 
+
+    // event: at specified timestamp, method to be executed number of times
+    // start(): execute once when started, initial setting
+
+    private void Start()
+    {
+        print("Hello Zen");
+    }
+}
